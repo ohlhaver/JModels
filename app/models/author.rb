c6afee2ab73a6b0154e140d59275051d5065ff2b
Ram Singla <ram.singla@gmail.com>
2009-11-27 15:39:40 +0530

    Fixing issue with Story Author

--- app/models/author.rb
+++ app/models/author.rb
@@ -12,6 +12,24 @@ class Author < ActiveRecord::Base
   after_save :set_story_delta_flag
   after_destroy :set_story_delta_flag
   
+  def self.create_or_find(author_names)
+    authors = []
+    author_names.each do |a_n|
+      a_n = a_n[0, 100] # author names are truncated at 100 chars
+      a = self.find( a_n )
+      a_n = a_n.chars.upcase.to_s
+      a ||= self.find_or_initialize_by_name( a_n )
+      if a.new_record?
+        a.is_agency = false
+        a = a.save ? a : nil
+      end
+      authors.push( a ) if a
+    end
+    return authors
+  end
+  
+  protected
+  
   def set_delta_index_story
     @delta_index_story = name_changed?
     return true
--- app/models/story_author.rb
+++ app/models/story_author.rb
@@ -2,9 +2,16 @@ class StoryAuthor < ActiveRecord::Base
   belongs_to  :story
   belongs_to  :author
   
-  before_save :set_story_delta_flag
+  before_save :set_delta_index_story
   after_save :set_story_delta_flag
   after_destroy :set_story_delta_flag
+  
+  protected
+  
+  def set_delta_index_story
+    @delta_index_story = name_changed?
+    return true
+  end
   
   def set_story_delta_flag
     story.update_attribute( :delta, true ) if frozen? || @delta_index_story
