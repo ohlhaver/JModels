954e293658a31e54fe4370d6aef00d965560ad94
Ram Singla <ram.singla@gmail.com>
2010-01-18 17:46:49 +0530

    Duplicate Marker BugFix

--- app/services/duplicate_marker.rb
+++ app/services/duplicate_marker.rb
@@ -52,6 +52,8 @@ class DuplicateMarker < BackgroundService
       db.execute( DB::Insert::Ignore + 'INTO duplicate_stories (id, master_id) SELECT id, NULL FROM candidate_stories' )
     end
     
+    logger.info( 'Duplicate Stories with Correct Leader Found. Updating the Background and MasterDB' )
+    
     #
     # Step 5: Candidate Stories Update
     #
@@ -62,8 +64,8 @@ class DuplicateMarker < BackgroundService
     #
     # Step 6: Master DB Update
     #
-    master_db.transaction do
-      DuplicateStory.find_each do |story|
+    DuplicateStory.find_each do |story|
+      master_db.transaction do
         next unless story.master_id
         master_db.execute( MasterDB::Insert::Ignore + 'INTO story_metrics ( story_id ) VALUES(' + db.quote( story.id ) + ')')
         story_metric = StoryMetric.find( :first, :conditions => { :story_id => story.id  } )
@@ -112,8 +114,9 @@ class DuplicateMarker < BackgroundService
       
       story_ids = group.stories.all( :select => 'id' ).collect{ |x| x.id }
       
-      db.execute( 'INSERT INTO candidate_story_keywords ( story_id, keyword_id, frequency ) SELECT keyword_subscriptions.story_id, keyword_subscriptions.keyword_id, keyword_subscriptions.frequency
-      FROM keyword_subscriptions WHERE keyword_subscriptions.story_id IN (' + story_ids.join(',') + ')' )
+      db.execute( 'INSERT INTO candidate_story_keywords ( story_id, keyword_id, frequency ) 
+        SELECT keyword_subscriptions.story_id, keyword_subscriptions.keyword_id, keyword_subscriptions.frequency
+        FROM keyword_subscriptions WHERE keyword_subscriptions.story_id IN (' + story_ids.join(',') + ')' )
       
       story_ids_groups = db.select_values( 'SELECT GROUP_CONCAT( story_id ) FROM candidate_story_keywords GROUP by keyword_id' )
       
@@ -140,14 +143,16 @@ class DuplicateMarker < BackgroundService
     pair_hash.each do | s1_id, s1_hash |
       db.transaction do
         s1_hash.each do | s2_id, a_int_b_frequency |
+          next if pair_hash[s1_id][s1_id].zero? || pair_hash[s2_id][s2_id].zero?
           a_union_b_frequency = pair_hash[s1_id][s1_id] + pair_hash[s2_id][s2_id] - a_int_b_frequency
-          next if (a_int_b_frequency.to_f / a_union_b_frequency.to_f) < 0.90
+          next if (a_int_b_frequency * 100 )/a_union_b_frequency < 90
           db.execute( DB::Insert::Ignore + 'INTO candidate_similarities (story1_id, story2_id, frequency ) VALUES( ' +
             db.quote_and_merge( s1_id, s2_id, pair_hash[s1_id][s1_id] ) + ')' ) ## Inserting s1 frequency
         end
       end
     end
     
+    pair_hash.each{ |k,v| v.clear }.clear
     logger.info( 'Candidate Similarities Table Size: ' + db.select_value( 'SELECT COUNT(*) FROM candidate_similarities' ) + ' Rows.' )
     
     #
@@ -167,86 +172,4 @@ class DuplicateMarker < BackgroundService
     
     
   end
-  
-  def populate_candidate_story_keywords_old
-    
-    #
-    # Step 1: Candidate Story Similarities
-    #
-    
-    # db.create_table :shadow_candidate_similarities, :force => true, :id => false do |t|
-    #   t.integer :story1_id
-    #   t.integer :story2_id
-    #   t.integer :frequency
-    # end
-    # 
-    # db.add_index :shadow_candidate_similarities, [ :story1_id, :story2_id ], :unique => true
-    # 
-    # db.execute( 'INSERT INTO shadow_candidate_similarities (story1_id, story2_id, frequency) SELECT story1_id, story2_id, frequency FROM candidate_similarities' )
-    # 
-    # db.execute( 'DELETE * FROM candidate_similarities' )
-    
-    # Finding Duplicate Stories Inside the Story Group
-    
-    db.execute('DELETE FROM candidate_similarities WHERE story1_id NOT IN ( SELECT candidate_stories.id FROM candidate_stories )')
-    db.execute('DELETE FROM candidate_similarities WHERE story2_id NOT IN ( SELECT candidate_stories.id FROM candidate_stories )')
-    
-    StoryGroup.current_session.find_each do |group|
-      story_ids = group.stories.all( :select => 'id' ).collect{ |x| x.id }
-      story_ids.each do |s1_id|
-        story_ids.each do |s2_id|
-          db.execute( DB::Insert::Ignore + 'INTO candidate_similarities (story1_id, story2_id) VALUES (' + db.quote_and_merge( s1_id, s2_id ) + ')' )
-        end
-      end
-    end
-    return if exit?
-    
-    # db.execute( 'UPDATE candidate_similarities SET frequency = ( SELECT s.frequency FROM shadow_candidate_similarities AS s 
-    #   WHERE s.story1_id = candidate_similarities.story1_id AND s.story2_id = candidate_similarities.story2_id )' )
-    # 
-    # db.drop_table( :shadow_candidate_similarities )
-    
-    db.execute( 'UPDATE candidate_similarities SET frequency = ( SELECT COUNT(*) FROM keyword_subscriptions WHERE story_id = story1_id ) 
-      WHERE story1_id = story2_id AND frequency IS NULL' )
-    
-    db.create_table( 'story_keyword_ids', :force => true ) do |t|
-    end
-    
-    story_ids = db.select_values( 'SELECT story1_id FROM candidate_similarities WHERE frequency IS NULL GROUP BY story1_id' )
-    
-    story_ids.each do | story_id |
-      break if exit?
-      db.execute( 'DELETE FROM story_keyword_ids' )
-      db.execute(  DB::Insert::Ignore + 'INTO story_keyword_ids (id) SELECT keyword_id FROM keyword_subscriptions WHERE story_id = ' + db.quote( story_id ) )
-      db.execute( 'UPDATE candidate_similarities SET frequency = ( SELECT COUNT(*) FROM story_keyword_ids 
-          INNER JOIN keyword_subscriptions ON ( story_keyword_ids.id = keyword_subscriptions.keyword_id ) 
-          WHERE story_id = candidate_similarities.story2_id ) 
-        WHERE frequency IS NULL AND story1_id = ' + db.quote( story_id ) )
-    end
-    return if exit?
-    
-    db.drop_table( 'story_keyword_ids' )
-    
-    logger.info( 'Candidate Similarities Table Size: ' + db.select_value( 'SELECT COUNT(*) FROM candidate_similarities' ) + ' Rows.' )
-    
-    #
-    # Step 2: Generate Duplicate Stories Groups
-    #
-    db.create_table( 'duplicate_groups', :force => true, :id => false ) do |t|
-      t.integer :story_id
-      t.integer :master_id
-      t.integer :frequency # keyword frequency count in story_id
-    end
-    
-    db.execute( 'INSERT INTO duplicate_groups ( story_id, master_id, frequency )
-      SELECT ss1.story1_id as story_id, ss1.story2_id as master_id, ss2.frequency as frequency
-      FROM candidate_similarities AS ss1 
-      LEFT OUTER JOIN candidate_similarities AS ss2 ON ( ss1.story1_id = ss2.story1_id  AND ss1.story1_id = ss2.story2_id ) 
-      LEFT OUTER JOIN candidate_similarities AS ss3 ON ( ss1.story2_id = ss3.story1_id  AND ss1.story2_id = ss3.story2_id )
-      WHERE ( ss1.frequency IS NOT NULL )
-        AND ( ss1.frequency / ( ss2.frequency + ss3.frequency - ss1.frequency )  >= 0.90 )' ) #  |A intersection B| / |A union B|
-          
-    db.add_index 'duplicate_groups', [ :story_id, :master_id ], :name => 'dup_grp_idx'
-  end
-  
 end
