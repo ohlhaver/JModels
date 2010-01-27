306e7738335639445e3c106b627ab865959a0544
Ram Singla <ram.singla@gmail.com>
2010-01-27 17:13:34 +0530

    Skipping Delta Update on StoryGroup Update

--- app/models/story_group_membership.rb
+++ app/models/story_group_membership.rb
@@ -5,13 +5,13 @@ class StoryGroupMembership < ActiveRecord::Base
   belongs_to :story
   belongs_to :story_group, :class_name => 'StoryGroup', :foreign_key => :group_id
   
-  after_create  :set_story_delta_flag
-  after_destroy :set_story_delta_flag
+  #after_create  :set_story_delta_flag
+  #after_destroy :set_story_delta_flag
   
   protected
     
-  def set_story_delta_flag
-    story.update_attribute( :delta, true ) if frozen?
-  end
+  #def set_story_delta_flag
+  #  story.update_attribute( :delta, true ) if frozen?
+  #end
   
 end
