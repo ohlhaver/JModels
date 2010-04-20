e2adf74d72660f3502fbd035d4af8d37411a08d2
Ram Singla <ram.singla@gmail.com>
2010-04-20 12:10:36 +0530

    Topic Email Alerts Bugfix

--- vendor/plugins/account_restrictions/lib/account_restriction.rb
+++ vendor/plugins/account_restrictions/lib/account_restriction.rb
@@ -12,6 +12,7 @@ module ActiveRecord
         raise ArgumentError if options[:user].blank? || options[:association].blank?
         
         class_eval do
+          
           cattr_accessor :user_account_restrictions
           self.user_account_restrictions ||= Hash.new
           self.user_account_restrictions[:user] = options[:user]
@@ -21,6 +22,7 @@ module ActiveRecord
             include RestrictionMethods
             alias_method_chain :find, :account_restriction
             alias_method_chain :count, :account_restriction
+            alias_method_chain :with_scope, :account_restriction
           end
           include InstanceMethods
         end  
@@ -29,9 +31,36 @@ module ActiveRecord
     end
     
     module RestrictionMethods
+                             
+      def with_scope_with_account_restriction( method_scoping = {}, action = :merge, &block )
+        method_scoping = method_scoping.method_scoping if method_scoping.respond_to?(:method_scoping)
+        # Dup first and second level of hash (method and params).
+        method_scoping = method_scoping.inject({}) do |hash,   (method, params)|
+          hash[method] = (params == true) ? params : params.  dup
+          hash                                                
+        end
+        user = nil
+        if f = method_scoping[:find]
+          user = f.delete(:user)
+        end
+        if user
+          hash = { :find => { :user => user } }
+          current_scoped_methods.inject(hash) do | hash, (method, params)|
+            hash[method] = params
+          end if current_scoped_methods
+          hash[:find].merge!( :user => user )
+          self.scoped_methods << hash
+        end
+        begin
+          with_scope_without_account_restriction( method_scoping, action, &block )
+        ensure
+          self.scoped_methods.pop if user
+        end
+      end
+      
       def find_with_account_restriction( *args )
         options = args.extract_options!
-        user = options.delete(:user)
+        user = options.delete(:user) || scope(:find, :user)
         options.merge!( :limit => 1 ) unless user && user.power_plan?
         args.push( options )
         find_without_account_restriction( *args )
@@ -39,7 +68,7 @@ module ActiveRecord
       
       def count_with_account_restriction( *args )
         options = args.extract_options!
-        user = options.delete(:user)
+        user = options.delete(:user) || scope(:find, :user)
         args.push( options )
         count = count_without_account_restriction( *args )
         user && user.power_plan? ? count : ( count > 0 ? 1 : 0 )
@@ -60,10 +89,30 @@ module ActiveRecord
     
     module AssociationMethods
       
+      def find_in_batches( *args )
+        options = args.extract_options!
+        options[:user] = proxy_owner
+        args.push( options )
+        super(*args)
+      end
+      
+      def find_each( *args )
+        options = args.extract_options!
+        options[:user] = proxy_owner
+        args.push( options )
+        super(*args)
+      end
+      
+      def paginate( *args )
+        options = args.extract_options!
+        options[:user] = proxy_owner
+        args.push( options )
+        super(*args)
+      end
+      
       def all( *args )
         options = args.extract_options!
         options[:user] = proxy_owner
-        args.push( :all ) if args.empty?
         args.push( options )
         super(*args)
       end
