884f10d4056c0d78f114aecb1195f9082ccd129e
Ram Singla <ram.singla@gmail.com>
2010-05-20 18:40:28 +0530

    Adding Index to Speed Up Sphinx Indexing

---
+++ db/migrate/049_add_story_table_index.rb
@@ -0,0 +1,11 @@
+class AddStoryTableIndex < ActiveRecord::Migration
+  def self.up
+    add_index :stories, [ :delta, :created_at ], :name => 'stories_sphinx_idx'
+    remove_index :stories, :name => 'stories_created_at_idx'
+  end
+  
+  def self.down
+    remove_index :stories, [ :delta, :created_at ], :name => 'stories_sphinx_idx'
+    add_index :stories, :created_at, :name => 'stories_created_at_idx'
+  end
+end
