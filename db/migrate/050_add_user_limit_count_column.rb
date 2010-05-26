d5bf2580aa28e4ddcae9c2323bc3e4f208d66bef
Ram Singla <ram.singla@gmail.com>
2010-05-26 12:29:30 +0530

    Migration Fix

--- db/migrate/050_add_user_limit_count_column.rb
+++ db/migrate/050_add_user_limit_count_column.rb
@@ -1,8 +1,8 @@
 class AddUserLimitCountColumn < ActiveRecord::Migration
   def self.up
-    #add_column :users, :author_count, :integer, :default => 0
-    #add_column :users, :source_count, :integer, :default => 0
-    #add_column :users, :topic_count, :integer, :default => 0
+    add_column :users, :author_count, :integer, :default => 0
+    add_column :users, :source_count, :integer, :default => 0
+    add_column :users, :topic_count, :integer, :default => 0
     User.find_each do |user|
       user.author_count = user.author_subscriptions.count(:all, :without_account_restriction => true )
       user.source_count = user.source_subscriptions.count(:all, :without_account_restriction => true )
