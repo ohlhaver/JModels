68a84600c0e2849936d97e0630465eb0563aa9da
Ram Singla <ram.singla@gmail.com>
2010-06-14 11:21:33 +0530

    Duplicate Checker Bugfix

--- app/runners/duplicate_checker.rb
+++ app/runners/duplicate_checker.rb
@@ -16,7 +16,7 @@ class DuplicateChecker < BackgroundRunnerPool
         DuplicateDeletion.new( :logger => self.logger, :parent => self ).run( :with_session => true, :start => { :test => true } )
       end
     else
-      add_runner( 'Duplicate Table Trimmer',  :run_daily ) do
+      add_runner( 'Duplicate Table Trimmer',  :run_every_day ) do
         bm = Benchmark.measure{  StoryTitle.purge! }
         self.logger.info( "Background Service Benchmark: Duplicate Table Trimmer:\n" + Benchmark::Tms::CAPTION + bm.to_s )
       end
